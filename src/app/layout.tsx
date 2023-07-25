0781d6bf5b3ae27c27070626cd091be85754430a
Donald Ivan Cribillo <developer.donaldivan@gmail.com>
2023-07-25 21:13:52 +0800

    authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
+import { Toaster } from "@/components/ui/Toaster";
 
 export const metadata = {
   title: "Breadit",
@@ -24,6 +25,7 @@ const Layout = ({ children }: AppWithChildrenProps) => {
         <div className="container h-full pt-12 mx-auto max-w-7xl">
           {children}
         </div>
+        <Toaster />
       </body>
     </html>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
 import { Icons } from "./Icons";
 import { buttonVariants } from "./ui/Button";
+import { getAuthSession } from "@/lib/auth";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await getAuthSession();
   return (
     <div className="fixed inset-x-0 top-0 h-fit bg-zinc-100 border-b border-zinc-300  z-[10] py-2">
       <div className="container flex justify-between h-full gap-2 mx-auto max-w-7xl item-center">
