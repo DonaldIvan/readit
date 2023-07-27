c5b230f8883822782a271d30990ee477efbda66e
Donald Ivan Cribillo <developer.donaldivan@gmail.com>
2023-07-27 21:50:33 +0800

    displaying posts

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -89,15 +89,17 @@ const Layout: FC<Props> = async ({ children, params: { slug } }) => {
                   subredditName={subreddit.name}
                 />
               ) : null}
-              <Link
-                className={buttonVariants({
-                  variant: "outline",
-                  className: "w-full mb-6",
-                })}
-                href={`/r/${slug}/submit`}
-              >
-                Create Post
-              </Link>
+              {session?.user && (
+                <Link
+                  className={buttonVariants({
+                    variant: "outline",
+                    className: "w-full mb-6",
+                  })}
+                  href={`/r/${slug}/submit`}
+                >
+                  Create Post
+                </Link>
+              )}
             </dl>
           </div>
         </div>
--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,13 +1,16 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { FC } from "react";
+import { SessionProvider } from "next-auth/react";
 
 interface Props extends AppWithChildrenProps {}
 
 const Providers: FC<Props> = ({ children }) => {
   const queryClient = new QueryClient();
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider>{children}</SessionProvider>
+    </QueryClientProvider>
   );
 };
 
